76bc8fed9eef4eee374c17c2e1cbb30eced80efe
110101001 <pw434@cornell.edu>
2021-11-28 15:20:04 -0500

    change robot control

--- Feature_Capture/src/robot_control.cpp
+++ Feature_Capture/src/robot_control.cpp
@@ -3,41 +3,37 @@
 #include <time.h>
 #include <unistd.h>
 
-void moveForward(int speed, int time){
+void initRobot(){
+    initialMotors();
+}
+
+void moveForward(int speed){
     //speed 0-255
     setLeftMode(2);
     setRightMode(1);
     setLeftSpeed(speed);
     setRightSpeed(speed);
-    sleep(time);
-    stop();
 }
 
-void moveBackward(int speed, int time){
+void moveBackward(int speed){
     setLeftMode(1);
     setRightMode(2);
     setLeftSpeed(speed);
     setRightSpeed(speed);
-    sleep(time);
-    stop();
 }
 
-void turnLeft(int angle){
+void turnLeft(int speed){
     setLeftMode(1);
     setRightMode(1);
-    setLeftSpeed(200);
-    setRightSpeed(200);
-    sleep(angle);
-    stop();
+    setLeftSpeed(speed);
+    setRightSpeed(speed);
 }
 
-void turnRight(int angle){
+void turnRight(int speed){
     setLeftMode(2);
     setRightMode(2);
-    setLeftSpeed(255);
-    setRightSpeed(255);
-    sleep(angle);
-    stop();
+    setLeftSpeed(speed);
+    setRightSpeed(speed);
 }
 
 void robotStop(){
